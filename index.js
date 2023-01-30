e06dd4122c67eb8a8fc36d55f176a268b61eb4b0
NashiDetki <93508687+NashiDetki@users.noreply.github.com>
2023-01-30 15:56:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+let image = document.querySelector('.viewer_image');
+console.log(image);
+
+
+let link1= document.querySelector('.link1');
+console.log(link1);
+
+let link2=document.querySelector('.link2');
+console.log(link2);
+
+let link3=document.querySelector('.link3');
+console.log(link3);
+
+let link4=document.querySelector('.link4');
+console.log(link4);
+
+link1.addEventListener('click', function(){
+    image.src="./img/sjet100 aur";
+})
+link2.addEventListener('click ',function(){
+image.src=image.spc="./img/sjet100 aur";
+} )
+link3.addEventListener('click', function(){
+image.src=image.spc="./img/sjet100 aur";
+})
+link4.addEventListener('click', function(){
+
+})
